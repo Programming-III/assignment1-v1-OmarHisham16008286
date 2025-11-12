6b1c44296a4075492dfa90f6dccc927d2c91fbfa
OmarHisham16008286 <avatargamer150@gmail.com>
2025-11-12 15:46:04 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,28 @@
-#ifndef REPTILE_H
-#define REPTILE_H
-
+#ifndef Reptile_h
+#define Reptile_h
 #include "Animal.h"
+#include <iostream>
+using namespace std;
 
-//define reptile class here
+class Reptile : public Animal{
+
+    private: 
+    bool isVenomous;
+
+public: 
+Reptile();
+
+Reptile(string name,int age,bool isHungry,bool isVenomous);
+
+~Reptile();
+
+void display() override;
+void feed() override;
+
+void setIsVenomous(bool isVenomous);
+bool getIsVenomous();
 
 
 
-
-
-
-
-
-
+}; 
 #endif
